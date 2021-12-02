206c9c9b84a79ac716b270a7beac07b1792024e4
Brian Rowlett <bjrowlett2@gmail.com>
2021-12-02 00:17:53 -0600

    Added solution for Day 02

---
+++ src/bin/day_02.rs
@@ -0,0 +1,108 @@
+mod aoc;
+
+enum Action {
+    Up,
+    Down,
+    Forward,
+}
+
+struct Command {
+    action: Action,
+    amount: i32,
+}
+
+struct Day02 {
+    commands: Vec<Command>,
+}
+
+fn main() {
+    let mut day = Day02 {
+        commands: vec![],
+    };
+
+    for line in aoc::lines("inputs/day_02.txt") {
+        match parse_command(&line) {
+            Ok(value) => day.commands.push(value),
+            Err(reason) => panic!("parse_command failed: {}", reason),
+        };
+    }
+
+    match solve_part_1(&day) {
+        Ok(value) => println!("Part 1: {}", value),
+        Err(reason) => panic!("Part 1 failed: {}", reason),
+    };
+
+    match solve_part_2(&day) {
+        Ok(value) => println!("Part 2: {}", value),
+        Err(reason) => panic!("Part 2 failed: {}", reason),
+    };
+}
+
+fn parse_action(action: &str) -> Action {
+    return match action {
+        "up" => Action::Up,
+        "down" => Action::Down,
+        "forward" => Action::Forward,
+        unknown => panic!("Unknown action '{}'", unknown),
+    };
+}
+
+fn parse_amount(amount: &str) -> i32 {
+    return match amount.parse::<i32>() {
+        Ok(value) => value,
+        Err(reason) => panic!("String::parse failed: {}", reason),
+    };
+}
+
+fn parse_command(line: &str) -> Result<Command, String> {
+    let mut parts = line.split(" ");
+
+    return Ok(Command {
+        action: match parts.next() {
+            Some(value) => parse_action(&value),
+            None => return Err(String::from("No action found")),
+        },
+        amount: match parts.next() {
+            Some(value) => parse_amount(&value),
+            None => return Err(String::from("No amount found")),
+        },
+    });
+}
+
+fn solve_part_1(day: &Day02) -> Result<i32, String> {
+    let mut depth = 0;
+    let mut horizontal = 0;
+    
+    for command in &day.commands {
+        match command.action {
+            Action::Up => depth -= command.amount,
+            Action::Down => depth += command.amount,
+            Action::Forward => horizontal += command.amount,
+        };
+    }
+
+    return Ok(depth * horizontal);
+}
+
+fn solve_part_2(day: &Day02) -> Result<i32, String> {
+    let mut aim = 0;
+    let mut depth = 0;
+    let mut horizontal = 0;
+
+    for command in &day.commands {
+        match command.action {
+            Action::Up => {
+                aim -= command.amount;
+            },
+            Action::Down => {
+                aim += command.amount;
+            },
+            Action::Forward => {
+                horizontal += command.amount;
+                depth += aim * command.amount;
+            },
+        };
+    }
+
+    return Ok(depth * horizontal);
+}
