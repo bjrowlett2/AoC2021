8b196988c33557708c072e38b7e822c181c07eda
Brian Rowlett <bjrowlett2@gmail.com>
2021-12-02 00:40:39 -0600

    Refactored solution for Day 02

--- src/bin/day_02.rs
+++ src/bin/day_02.rs
@@ -8,7 +8,7 @@ enum Action {
 
 struct Command {
     action: Action,
-    amount: i32,
+    amount: i64,
 }
 
 struct Day02 {
@@ -21,20 +21,17 @@ fn main() {
     };
 
     for line in aoc::lines("inputs/day_02.txt") {
-        match parse_command(&line) {
-            Ok(value) => day.commands.push(value),
-            Err(reason) => panic!("parse_command failed: {}", reason),
-        };
+        day.commands.push(parse_command(&line));
     }
 
     match solve_part_1(&day) {
         Ok(value) => println!("Part 1: {}", value),
-        Err(reason) => panic!("Part 1 failed: {}", reason),
+        Err(reason) => panic!("solve_part_1 failed: {}", reason),
     };
 
     match solve_part_2(&day) {
         Ok(value) => println!("Part 2: {}", value),
-        Err(reason) => panic!("Part 2 failed: {}", reason),
+        Err(reason) => panic!("solve_part_2 failed: {}", reason),
     };
 }
 
@@ -43,33 +40,33 @@ fn parse_action(action: &str) -> Action {
         "up" => Action::Up,
         "down" => Action::Down,
         "forward" => Action::Forward,
-        unknown => panic!("Unknown action '{}'", unknown),
+        unknown => panic!("parse_action failed: {}", unknown),
     };
 }
 
-fn parse_amount(amount: &str) -> i32 {
-    return match amount.parse::<i32>() {
+fn parse_amount(amount: &str) -> i64 {
+    return match amount.parse::<i64>() {
         Ok(value) => value,
         Err(reason) => panic!("String::parse failed: {}", reason),
     };
 }
 
-fn parse_command(line: &str) -> Result<Command, String> {
+fn parse_command(line: &str) -> Command {
     let mut parts = line.split(" ");
 
-    return Ok(Command {
+    return Command {
         action: match parts.next() {
             Some(value) => parse_action(&value),
-            None => return Err(String::from("No action found")),
+            None => panic!("parse_command failed: No action found"),
         },
         amount: match parts.next() {
             Some(value) => parse_amount(&value),
-            None => return Err(String::from("No amount found")),
+            None => panic!("parse_command failed: No amount found"),
         },
-    });
+    };
 }
 
-fn solve_part_1(day: &Day02) -> Result<i32, String> {
+fn solve_part_1(day: &Day02) -> Result<i64, String> {
     let mut depth = 0;
     let mut horizontal = 0;
     
@@ -84,7 +81,7 @@ fn solve_part_1(day: &Day02) -> Result<i32, String> {
     return Ok(depth * horizontal);
 }
 
-fn solve_part_2(day: &Day02) -> Result<i32, String> {
+fn solve_part_2(day: &Day02) -> Result<i64, String> {
     let mut aim = 0;
     let mut depth = 0;
     let mut horizontal = 0;
